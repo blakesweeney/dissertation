588477c9bf2d6174b7cc584397f34a85255627f2
Blake Sweeney <blakes.85@gmail.com>
2016-11-15 23:44:07 -0500

    Update editor and use latexmk

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
-NAME=dissertation
 TEXSRCS=$(wildcard chapter-*/body.tex)
+MAIN=dissertation.tex
+MISC=$(wildcard misc/*.tex)
 BIBTEXSRCS=all.bib
-USE_PDFLATEX=1
 
-include /Users/bsweene/.local/share/latex-mk/latex.gmk
+all : pdf
+
+clean:
+	latexmk -C
+
+pdf : $(TEXSRCS) $(MAIN) $(BIBTEXSRCS) $(MISC)
+	latexmk -pdf -pdflatex="pdflatex --shell-escape %O %S"
--- default.nix
+++ default.nix
@@ -2,12 +2,14 @@ let
   pkgs = import <nixpkgs> {};
     stdenv = pkgs.stdenv;
       dev = import /Users/bsweene/.nixpkgs/pkgs/dev.nix {};
+      editor = import /Users/bsweene/.nixpkgs/pkgs/editor.nix { pkgs=pkgs; };
   in rec  {
       env = stdenv.mkDerivation {
         name = "env";
         buildInputs = [
           pkgs.texLiveFull
           pkgs.ghostscript
+          editor.editor
         ];
       };
   }
